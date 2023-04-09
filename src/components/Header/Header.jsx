49a416fa9e5da33e050bc569a5e032e804cbb427
unknown <oakhmadjonov1@gmail.com>
2023-04-09 15:23:04 +0500

    header done and banner background image added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/home/logo.png";
+import { FaUserAlt, FaSearch, FaHeart, FaShoppingCart } from "react-icons/fa";
 import "./header.scss";
 const Header = () => {
   return (
     <div className="header">
-      <h1>Header</h1>
+      <div className="header__container">
+        <nav>
+          <div className="menu">
+            <Link className="menu__link">HOME</Link>
+            <Link className="menu__link">ABOUT</Link>
+            <Link className="menu__link">SHOP</Link>
+            <Link className="menu__link">BLOGS</Link>
+            <Link className="menu__link">CONTACT</Link>
+          </div>
+          <div className="logo">
+            <Link className="logo_img">
+              <img src={logo} alt="Logo" />
+            </Link>
+          </div>
+          <div className="login__search__like__cart">
+            <Link className="login">
+              <FaUserAlt /> LOGIN
+            </Link>
+            <Link className="search">
+              {" "}
+              <FaSearch />
+              SEARCH...
+            </Link>
+            <Link className="like">
+              <FaHeart /> (0)
+            </Link>
+            <Link className="cart">
+              <FaShoppingCart /> (0)
+            </Link>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import "./home.scss";
 const Home = () => {
   return (
     <div className="home">
-      <h1>Home Page</h1>
+      <div className="banner"></div>
     </div>
   );
 };
